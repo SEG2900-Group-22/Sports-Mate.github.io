6a2910d939a802b1f3286d3520d863dc02acdcb6
Farina Salman <farinasalman@gmail.com>
2019-10-22 19:21:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-/* Automatic slideshow */
-var slideIndex = 0;
-showSlides();
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
 
-function showSlides() {
+function carousel() {
   var i;
-  var slides = document.getElementsByClassName("mySlides fade");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 3000);}
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
 
   /* Navigation bar */
   function myFunction() {
