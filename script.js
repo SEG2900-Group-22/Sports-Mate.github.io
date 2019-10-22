3378c6c263755dac21b7a8b3552a2310116bac6c
John Doe <johndoe@example.c>
2019-10-22 19:44:49 -0400

    slideshow attempt

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
 
-function showSlides() {
+function currentSlide(n){
+  alert("ok")
+  showSlides(slideIndex=n)
+  
+
+}
+function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides fade");
+  alert(slides.length)
+  if (n > slides.length) {slideIndex = 1} 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 3000);}
+  if (n < 1) {slideIndex = slides.length}
+  
+  
+  
+  slides[slideIndex-1].style.display = "block"; }
 
   /* Navigation bar */
   function myFunction() {
