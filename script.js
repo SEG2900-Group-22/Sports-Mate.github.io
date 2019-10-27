cd6e044ec4619c0f31194284f7e1d7af5c3a73c6
John Doe <johndoe@example.c>
2019-10-26 20:53:01 -0400

    Home Page done

--- script.js
+++ script.js
@@ -12,4 +12,66 @@ var myIndex = 0;
       x.className = "topNav";
     }
   }
+  function showLogin(){
+    
+   
+    $(".content").css("filter","blur(2px)");
+   
+    
+    $(".form-popup").show();
+    
+   
+
+}
+function closeLogin(){
+    Document.getElementsByClassName("form-popup").style.display="none";
+
+}       
+var myIndex = 0;
+            
+var lastIndex=0;
+var x = document.getElementsByClassName("mySlides");
+var dots=document.getElementsByClassName("dot"); 
+currentIndex(1) ;
+var c=setInterval(carousel,6000);
+ 
+function currentIndex(n){
+    myIndex=n
+    dots[lastIndex].className=dots[lastIndex].className.replace(" active","");
+    x[lastIndex].style.display="none";
+    
+    showSlide();
+    
+   
+    setTimeout(showSlide,6000)
+}
+
+function carousel(){
+   
+
+   
+    if(myIndex>=document.getElementsByClassName('mySlides').length){myIndex=0;}
+    
+    myIndex++;
+   
+   showSlide();
+   setTimeout(showSlide,6000); 
   
+    
+}
+
+function showSlide() {
+changeActive()
+
+x[myIndex-1].style.display="block";
+
+dots[myIndex-1].className+=" active";
+lastIndex=myIndex-1; 
+
+}
+function changeActive(){
+    dots[lastIndex].className=dots[lastIndex].className.replace(" active","");
+    x[lastIndex].style.display="none"; 
+}
+
+
